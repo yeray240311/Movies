b2bd701e149ddd3c501e5d2374016e8cc17ef3f2
Yeray Naum Pech Pineda <22470166@campeche.tecnm.mx>
2026-05-14 09:32:49 -0600

    Agrega CRUD para las peliculas

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MoviesContext") ?? throw new InvalidOperationException("Connection string 'MoviesContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
